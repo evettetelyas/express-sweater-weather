1b0bb0ef72f593b473b3ef21fb8f0af1387e08e2
Evette Telyas <etelyas@gmail.com>
2019-11-18 23:51:47 -0700

    forecast endpoint

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const configuration = require('./knexfile')[environment];
 
 var indexRouter = require('./routes/index');
 var favoritesController = require('./app/controllers/favorites_controller')
+var forecastsController = require('./app/controllers/forecasts_controller')
 
 var app = express();
 
@@ -18,15 +19,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000);
 
 app.use('/', indexRouter);
 app.post('/api/v1/favorites', favoritesController.create)
 app.delete('/api/v1/favorites', favoritesController.destroy)
+app.get('/api/v1/forecast', forecastsController.show)
 
 
-// app.listen(app.get('port'), () => {
-// 	console.log(`${app.locals.title} is running`);
-//   });
+app.listen(app.get('port'), () => {
+	console.log(`${app.locals.title} is running`);
+  });
 
-module.exports = app;
+// module.exports = app;
