d7ec34633a564d5b69b2687fa1d894a33afca7e9
Evette Telyas <etelyas@gmail.com>
2019-11-18 17:05:14 -0700

    update production db url

--- knexfile.js
+++ knexfile.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: '',
+    connection: 'postgres://xinvbvsannovap:fa3db906686f1cec0805f21a359b64f290033092fcd60b7419d3983fc615b0b2@ec2-174-129-253-144.compute-1.amazonaws.com:5432/d1ubg2aibs7dfc',
     migrations: {
       directory: './db/migrations'
     },
