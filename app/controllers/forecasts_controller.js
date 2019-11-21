ec75a86551586edb8b04c28d472dd840577ff928
Evette Telyas <etelyas@gmail.com>
2019-11-20 20:34:09 -0700

    refactor forecast

--- app/controllers/forecasts_controller.js
+++ app/controllers/forecasts_controller.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 const fetch = require('node-fetch');
 const helper = require('../helpers/format_helper');
 const User = require('../models/user')
+const google_key = process.env.GOOGLE_API_KEY
+const google_base_url = "https://maps.googleapis.com/maps/api/geocode/json"
+const darksky_key = process.env.DARKSKY_API_KEY
+const darksky_url = `https://api.darksky.net/forecast/${darksky_key}/`
 
 const show = (request, response) => {
 	const location = request.param('location')
@@ -11,11 +15,7 @@ const show = (request, response) => {
 			if (user[0]) {
 			var city = location.split(",")[0]
 			var state = location.split(",")[1]
-			var google_key = process.env.GOOGLE_API_KEY
-			var google_base_url = "https://maps.googleapis.com/maps/api/geocode/json"
 			var google_url = `${google_base_url}?key=${google_key}&address=${city}+${state}`
-			var darksky_key = process.env.DARKSKY_API_KEY
-			var darksky_url = `https://api.darksky.net/forecast/${darksky_key}/`
 			fetch(google_url)
 			.then((res) => res.json())
 			.then((json) => {
